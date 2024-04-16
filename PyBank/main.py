a0e89923ac831755fecc1d44d549e32fab7878de
HGShelton <hannahgradyshelton@gmail.com>
2024-04-15 21:53:26 -0400

    initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,28 @@
+import os
+import csv
+
+# path to accept data from resources folder
+budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+# define parameters
+def print_add(budget_data):
+    months = str(budget_data[0])
+    pro_los = int(budget_data[1])
+
+# total months
+for month in months:
+    total_months = 0.00
+    total_months += months
+print(total_months)
+
+
+    
+
+
+
+
+
+
+    
+
+
