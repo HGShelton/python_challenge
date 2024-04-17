034957899d9d46494873e48d847c0db0e532785e
HGShelton <hannahgradyshelton@gmail.com>
2024-04-17 19:14:37 -0400

    Added changes in profit/losses.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,31 +1,24 @@
 import os
 import csv
 
-months = 0.00
-total = 0.00
+months = 0
+total = 0
+changes = []
 
 # path to accept data from resources folder
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 # read csv file
 with open(csvpath) as csvfile:
-
-    # specify delimiter
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    print(csvreader)
-
-    # read header row first
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    csvreader = csv.reader(csvfile, delimiter=',') # specify delimiter
+    csv_header = next(csvreader) # skip header row
+    
     # read each row after header
     for row in csvreader:
         
     # total months
         months +=1
     print(f"Total Months: {months}")
-
 
 # net total
 with open(csvpath) as csvfile:
@@ -34,26 +27,18 @@ with open(csvpath) as csvfile:
     for row in csv.reader(csvfile):
         total +=int(row[1])
     print(f"Total: ${total}")
-    
 
+# changes in profit/losses
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvfile)  
+    data = list(csvreader)
 
-    
+for i in range(1, months):
+    current_profit = int(data[i][1])
+    previous_profit = int(data[i-1][1])
+    change = current_profit - previous_profit
+    changes.append(change)
 
-        
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-    
-
+print(f"Changes: ${changes}")
 
