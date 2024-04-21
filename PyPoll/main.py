2ef204736f6bf7f0525710564ed5662f8045ca1b
HGShelton <hannahgradyshelton@gmail.com>
2024-04-21 10:46:14 -0400

    Fixed percentage votes printing issues. Added analysis folder to PyPoll.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,7 @@ can_list = []
 can_votes = {}
 winner = ""
 winner_votes = 0
+results = ""
 
 # path to accept data from resources folder
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -19,7 +20,6 @@ with open(csvpath) as csvfile:
 # total number of votes cast
     for row in csvreader:
         votes +=1
-    print(f'Total Votes: {votes}') #------------------------
 
 # list of condidates
 with open(csvpath) as csvfile:
@@ -32,12 +32,8 @@ with open(csvpath) as csvfile:
         if can not in can_list:
             can_list.append(can)
 
-# # print virtical list
-#     print(f"Candidates List:")
-# for can in can_list:
-#     print(can) --------------------------------------------------------------
-
 # calculate count and percentage of votes each candidate recieved 
+    # worked with peers in study group (Daniel, Zach, Nic) and utilized Xpert Learning Assistant to work through coding issues and bugs
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",") 
     csv_header = next(csvreader)
@@ -52,54 +48,22 @@ with open(csvpath) as csvfile:
     for can in can_list:
         vote = can_votes[can]
         perc_votes = (vote / votes) * 100
-        print(f"{can} : {perc_votes:.3f}% ({vote})")
-
-# for can, votes in can_votes.items(): #-------------------------------------------
-#     print(f"{can}: {votes} votes")
+        results += f"{can} : {perc_votes:.3f}% ({vote})\n"
 
 # winner
         if vote > winner_votes:
             winner = can
             winner_votes = vote
     print(f"{winner}")
+    
+    # print final election results
+        # Uilized Xpert Learning Assistant to fix percentage printing issue
+    print("Election Results")
+    print("--------------------------------")
+    print(f'Total Votes: {votes}')
+    print("--------------------------------")
+    print(results)
+    print("--------------------------------")
+    print(f'Winner: {winner}')
 
-
-
-
-              
-
-
-
-        # can = row['Candidate']
-        # if can in can_votes:
-        #     vote_count = vote_count + 1
-        # else:
-        #     vote_count = 1
-        # print(vote_count)
-        
-
-    # if can == can_list:
-    #     vote_count = (vote_count + 1)
-    # # else can not in can_list:
-    # print(vote_count)
-
-
-    # for name in can:
-    #     if name in vote_count:
-    #         vote_count[name] += 1
-    #     else:
-    #         vote_count[name] = 1
-
-    #     print(vote_count)
-
-
-
-      
-
-
-
-
-
-
-
-
+# output text file results to analysis folder
